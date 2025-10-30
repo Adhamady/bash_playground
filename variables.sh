26d2c5ddab8e0d87f7b067e0c43c2c4d0fea8133
adhamady <adhamamrkamalmady@gmail.com>
2025-10-31 01:05:31 +0200

    new file:   variables.sh

---
+++ variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "----------------------------------"
+echo "Welcome to bash variables tutorial"
+sleep 1
+echo "----------------------------------"
+
+NAME="ADHAM"
+AGE=24
+
+echo "Name : $NAME, Age:$AGE"
+
+
